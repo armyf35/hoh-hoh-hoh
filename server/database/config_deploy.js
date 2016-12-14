fad5d0c3d4cf4f677bbe7dff1b781151f3b20f98
Sam Phillips <armyf35@gmail.com>
2016-12-14 09:54:09 -0500

    Removed config.js

--- server/database/config_deploy.js
+++ server/database/config_deploy.js
@@ -1,12 +1,10 @@
 const mysql = require('mysql');
 
-const DBconfig = require('./config.js') || {};
-
 const connection = mysql.createConnection({
-  host: DBconfig.host || process.env.DB_HOST,
-  user: DBconfig.user || process.env.DB_USER,
-  password: DBconfig.password || process.env.DB_PASSWORD,
-  database: DBconfig.database || process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 module.exports = connection;
